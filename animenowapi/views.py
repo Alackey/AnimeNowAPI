f2210cf452455f8aea89fd06accd4d724ac42bed
Anthony Lackey <alackey96@gmail.com>
2016-06-22 01:45:47 -0700

    added /animeurl route

--- animenowapi/views.py
+++ animenowapi/views.py
@@ -9,10 +9,23 @@ def search():
 
         # Get anime that contain a keyword or match the given title
         animes_found = db.search(query=request.args.get("query"))
-        
+
         # Aggregate titles from animes found in database
         titles = []
         for anime in animes_found:
             titles.append(anime["title"])
 
         return jsonify({"titles": titles})
+
+
+@app.route("/videourl")
+def videourl():
+    if request.method == "GET":
+
+        # Get the anime information
+        anime = db.search(
+            query=request.args.get("query"),
+            is_title=True
+        )
+
+        return jsonify({"url": anime["url"]})
