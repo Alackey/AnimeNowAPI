02d700937b946a3aed389209252540e2503e7f58
Anthony Lackey <alackey96@gmail.com>
2016-06-22 18:17:26 -0700

    Start selenium code for getting video urls

--- animenowapi/__init__.py
+++ animenowapi/__init__.py
@@ -1,4 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+# Init selenium for getting videos
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+browser = webdriver.PhantomJS()
+
+browser.get("https://kissanime.to/")
+
+while browser.title == "Please wait 5 seconds...":
+    time.sleep(.1)
+
+
 import animenowapi.views
--- animenowapi/views.py
+++ animenowapi/views.py
@@ -1,5 +1,6 @@
-from animenowapi import app, db
+from animenowapi import app, browser, db
 from flask import request, jsonify
+from selenium.webdriver.common.keys import Keys
 
 
 # Get anime matching query
@@ -32,5 +33,14 @@ def videourl():
         # If not anime is found
         if anime is None:
             return jsonify({"url": "Error: Title not found", "Error": True})
+
+        print(browser.title)
+        # Open new window for selenium browser
+        body = browser.find_element_by_tag_name("body")
+        body.send_keys(Keys.CONTROL + 'n')
+        for handle in browser.window_handles:
+            print(handle)
+        browser.switch_to_window(browser.window_handles[-1])
+        print(browser.title)
 
         return jsonify({"url": anime["url"]})
