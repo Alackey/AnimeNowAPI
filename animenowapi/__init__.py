0afe22c303e9e415f3a04fd98ff1f9d0614aaa60
Anthony Lackey <alackey96@gmail.com>
2016-06-23 00:07:02 -0700

    Change to start browser for each /videourl call

--- animenowapi/__init__.py
+++ animenowapi/__init__.py
@@ -2,16 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# Init selenium for getting videos
-from selenium import webdriver
-import time
-
-browser = webdriver.PhantomJS()
-
-browser.get("https://kissanime.to/")
-
-while browser.title == "Please wait 5 seconds...":
-    time.sleep(.1)
-
-
 import animenowapi.views
--- animenowapi/views.py
+++ animenowapi/views.py
@@ -1,9 +1,11 @@
-from animenowapi import app, browser, db
+from animenowapi import app, db
 from flask import request, jsonify
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 # Get anime matching query
@@ -37,20 +39,30 @@ def videourl():
         if anime is None:
             return jsonify({"url": "Error: Title not found", "Error": True})
 
+
+        browser = webdriver.PhantomJS()
+
+        browser.get(anime["url"])
+
+        while browser.title == "Please wait 5 seconds...":
+            time.sleep(.1)
+
         print(browser.title)
-        # Open new window for selenium browser
-        # body = browser.find_element_by_tag_name("body")
-        # body.send_keys(Keys.CONTROL + 'n')
-        open_window_script = 'return window.open("' + anime["url"] + '", "any", \
-            "height = 450, width = 800, menubar=yes,scrollbars=yes, \
-            toolbar=yes,location=no,resizable=yes");'
-        handle = browser.execute_script(open_window_script)
-        # WebDriverWait(browser, 10).until(
-        #     EC.element_to_be_clickable((By.ID, "selectEpisode"))
-        # )
-        for handle in browser.window_handles:
-            print(handle)
-        browser.switch_to_window(browser.window_handles[-1])
-        print(browser.title)
+        # # Open new window for selenium browser
+        # # body = browser.find_element_by_tag_name("body")
+        # # body.send_keys(Keys.CONTROL + 'n')
+        # open_window_script = 'return window.open("' + anime["url"] + '", "any", \
+        #     "height = 450, width = 800, menubar=yes,scrollbars=yes, \
+        #     toolbar=yes,location=no,resizable=yes");'
+        # #browser.switch_to_window(browser.window_handles[1])
+        # handle = browser.execute_script(open_window_script)
+        # # WebDriverWait(browser, 30).until(
+        # #     EC.presence_of_element_located((By.ID, "selectEpisode"))
+        # # )
+        # time.sleep(10)
+        # for handle in browser.window_handles:
+        #     print(handle)
+        # browser.switch_to_window(browser.window_handles[-1])
+        # print(browser.title)
 
         return jsonify({"url": anime["url"]})
