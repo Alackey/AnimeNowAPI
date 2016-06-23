52125cbbc12923e24c49845f2616247b90f8d76d
Anthony Lackey <alackey96@gmail.com>
2016-06-22 22:57:05 -0700

    /videourl - Open new window for getting url

--- animenowapi/__init__.py
+++ animenowapi/__init__.py
@@ -4,9 +4,6 @@ app = Flask(__name__)
 
 # Init selenium for getting videos
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 browser = webdriver.PhantomJS()
--- animenowapi/views.py
+++ animenowapi/views.py
@@ -1,6 +1,9 @@
 from animenowapi import app, browser, db
 from flask import request, jsonify
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # Get anime matching query
@@ -36,8 +39,15 @@ def videourl():
 
         print(browser.title)
         # Open new window for selenium browser
-        body = browser.find_element_by_tag_name("body")
-        body.send_keys(Keys.CONTROL + 'n')
+        # body = browser.find_element_by_tag_name("body")
+        # body.send_keys(Keys.CONTROL + 'n')
+        open_window_script = 'return window.open("' + anime["url"] + '", "any", \
+            "height = 450, width = 800, menubar=yes,scrollbars=yes, \
+            toolbar=yes,location=no,resizable=yes");'
+        handle = browser.execute_script(open_window_script)
+        # WebDriverWait(browser, 10).until(
+        #     EC.element_to_be_clickable((By.ID, "selectEpisode"))
+        # )
         for handle in browser.window_handles:
             print(handle)
         browser.switch_to_window(browser.window_handles[-1])
